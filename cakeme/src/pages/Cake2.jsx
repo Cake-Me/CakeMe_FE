0faa37cf626ad180a614b0469088faf4792ed153
phjlia2430 <phjlia2430@naver.com>
2024-11-13 16:44:25 +0900

    [Feat] 다음페이지 버튼에 페이지2연결

--- cakeme/src/pages/Cake2.jsx
+++ cakeme/src/pages/Cake2.jsx
@@ -1,4 +1,13 @@
 import React from "react";
+import styled from "styled-components";
+import CakeDesign from "../components/cake/CakeDesign01"; 
+
+const CakeContainer = styled.div`
+  display: flex;
+
+  align-items: center;
+  
+`;
 
 const Cake2 = () => {
   return (
