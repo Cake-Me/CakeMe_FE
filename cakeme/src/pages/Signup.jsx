8b168ac124cc9917f03fb23061eb8e27db9e6838
phjlia2430 <phjlia2430@naver.com>
2025-02-08 00:07:35 +0900

    gitignore

--- cakeme/src/pages/Signup.jsx
+++ cakeme/src/pages/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
